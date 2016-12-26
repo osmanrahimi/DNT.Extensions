5fa150238250d7636b6a76c56559904035569c88
Rahimi <Rahimi@RAHIMI>
2016-12-26 15:50:26 +0330

    Add project files.

---
+++ DNT.OExtensions/SequentialGuidGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace DNT.OExtensions
+{
+    public static class SequentialGuidGenerator
+    {
+        #region Fileds
+
+        private static readonly RNGCryptoServiceProvider _rng = new RNGCryptoServiceProvider();
+
+        #endregion
+
+        #region Methods
+
+        #region New SequentialGuid
+
+        public static Guid NewSequentialGuid(SequentialGuidType guidType = SequentialGuidType.SequentialAsBinary)
+        {
+            byte[] randomBytes = new byte[10];
+            _rng.GetBytes(randomBytes);
+
+            long timestamp = DateTime.UtcNow.Ticks / 10000L;
+            byte[] timestampBytes = BitConverter.GetBytes(timestamp);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(timestampBytes);
+            }
+
+            byte[] guidBytes = new byte[16];
+
+            switch (guidType)
+            {
+                case SequentialGuidType.SequentialAsString:
+                case SequentialGuidType.SequentialAsBinary:
+                    Buffer.BlockCopy(timestampBytes, 2, guidBytes, 0, 6);
+                    Buffer.BlockCopy(randomBytes, 0, guidBytes, 6, 10);
+
+                    if (guidType == SequentialGuidType.SequentialAsString && BitConverter.IsLittleEndian)
+                    {
+                        Array.Reverse(guidBytes, 0, 4);
+                        Array.Reverse(guidBytes, 4, 2);
+                    }
+                    break;
+                case SequentialGuidType.SequentialAtEnd:
+                    Buffer.BlockCopy(randomBytes, 0, guidBytes, 0, 10);
+                    Buffer.BlockCopy(timestampBytes, 2, guidBytes, 10, 6);
+                    break;
+            }
+            return new Guid(guidBytes);
+        }
+
+        #endregion
+
+        #endregion
+    }
+
+    public enum SequentialGuidType
+    {
+        SequentialAsString,
+        SequentialAsBinary,
+        SequentialAtEnd
+    }
+}
